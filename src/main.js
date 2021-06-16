55bb31f9042982919fe680107979ec1f639f9d05
dwkim <dwkim@spiderkim.com>
2021-06-16 13:35:54 +0900

    chore: fix issue
    fill empty name as `John Doe`

--- src/main.js
+++ src/main.js
@@ -2,14 +2,8 @@ $(document).ready(function() {
     M.updateTextFields();
 });
   
-console.log($('#first_name2').val());
-
-// var input_elem = document.querySelector('#first_name2')
-// input_elem.addEventListener((e) => {
-//     console.log(e)
-// })
-
 $('#first_name2').on('input', function (e) {
-    var name = $(this).val()
-    $('.card-title').text(name)
+    var name = $(this).val();
+    if (name.length === 0) name = 'John Doe';
+    $('.card-title').text(name);
 })
