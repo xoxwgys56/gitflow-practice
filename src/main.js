0c1e71c9836af92f28d1cc5ee68c4f5fff7b47d5
dwkim <dwkim@spiderkim.com>
2021-06-16 11:54:02 +0900

    feat: add change name
    using jquery

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
     M.updateTextFields();
-  });
+});
+  
+console.log($('#first_name2').val());
+
+// var input_elem = document.querySelector('#first_name2')
+// input_elem.addEventListener((e) => {
+//     console.log(e)
+// })
+
+$('#first_name2').on('input', function (e) {
+    var name = $(this).val()
+    $('.card-title').text(name)
+})
